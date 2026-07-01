141529edc5a579cfa9a9911c8686765667e6ee93
Limalls <teodorolimadutra107@Gmail.com>
2026-07-01 17:10:21 -0300

    components

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,6 +1,30 @@
 import react from 'react'
+import {Code, Menu, Section, X} from 'lucide-react'
+import {navLinks} from '../../utils/constants'
 
-const navbar = () => {
+const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isScrolled, setIsScrolled] = useState(false)
+    const [activeSection = useScrollSpy(navLinks.map((link) => link.id))];
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const handleNavCLick = (SectionId) => {
+        scroolToSection(SectionId);
+        setIsMenuOpen(false);
+    }
+
+
     return (
         <div>Navbar</div>
     )
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,21 @@ export const PersonalInfo = {
     bio: 'i am a passionate web developer with a strong focus on front-end development. I have experience in building responsive and user-friendly websites using modern technologies such as React, JavaScript, HTML5, and CSS3. I am constantly learning and staying up-to-date with the latest trends in web development to deliver high-quality solutions.',
 }
 
+export const SocialLinks = {
+    github: "https://github.com/Limalls",
+}
+
+export const status = {
+    label: 'years of experience',
+    value: '3+',
+    label2: 'projects completed',
+    value2: '10+',
+}
+
+export const navLinks = [
+    { id: "home", label: "Home" },
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    {id: "projects", label: "Projects" },
+    { id: "contact", label: "Contact"}
+]
