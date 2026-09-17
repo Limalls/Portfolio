04b8230d39b26a529c7861ddb738d66496a86d12
Limalls <teodorolimadutra107@Gmail.com>
2026-09-17 16:02:52 -0300

    new adds

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import { useState } from 'react'
+import { ChevronDown, Star } from 'lucide-react'
+import { SiReact, SiNextdotjs, SiTailwindcss, SiNodedotjs, SiMongodb} from 'react-icons/si'
+import { PERSONAL_INFO } from '../../utils/constants'
+import { scrollToSection } from '../../hooks/useScrollSpy'
+import FadeIn from '../animations/Fadein'
+import RadialGradienteBackground from '../backgrounds/RadialGradienteBackground'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className=''>
+            <RadialGradienteBackground />
+
+
+    </section>
+
   )
 }
 
